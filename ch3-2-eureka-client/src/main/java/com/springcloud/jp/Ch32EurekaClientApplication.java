31d20f0b315b975312a3ab0c9a0a8fd1421d6ff0
JasonPeng <guo_tai@163.com>
2019-02-28 15:57:14 +0800

    init

---
+++ ch3-2-eureka-client/src/main/java/com/springcloud/jp/Ch32EurekaClientApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.jp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Ch32EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Ch32EurekaClientApplication.class,args);
+    }
+}
